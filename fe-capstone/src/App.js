e7b7161ea3b9ac8d4ef3efdf72548b12662720ef
shreyaDeb <shreya.deb@icloud.com>
2025-10-15 15:51:40 +0530

    app.js updated

--- fe-capstone/src/App.js
+++ fe-capstone/src/App.js
@@ -1,9 +1,22 @@
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Header from './components/header'
+import Footer from './components/footer'
+
+// Pages
+import HomePage from './HomePage';
+// import BookingPage from './pages/BookingPage';
+
+const App = () => {
   return (
-    <div className="App">
-      Homepage
+    <div className="min-h-screen bg-white">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/booking" element={<BookingPage />} /> */}
+      </Routes>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
